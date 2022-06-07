fd9352b8bb265f0ec0533f56534fb776bcb09509
noob-hash <80933227+noob-hash@users.noreply.github.com>
2022-06-07 06:19:00 +0545

    Delete doctor directory

--- doctor/dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"controller1","l":"controller"},{"p":"controller1","l":"model"},{"p":"<Unnamed>","l":"validateLogin"}];updateSearchResults();
